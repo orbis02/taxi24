b5820338deb3651ad8acc80e81f8bb25db29faff
Orbis Polanco <44383676+orbis02@users.noreply.github.com>
2023-02-22 09:01:34 -0400

    Create 1677003754302-migrationsconductores.ts

---
+++ dist/1677003754302-migrationsconductores.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class migrationsconductores1677003754302 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
